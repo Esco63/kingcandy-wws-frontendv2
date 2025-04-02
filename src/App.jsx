2d9eb5e069666bbcb7549e7536451b87b5e39e98
Esco63 <andre.michel1994@gmail.com>
2025-04-02 17:02:58 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Login from './pages/Login';
+import AdminDashboard from './pages/AdminDashboard';
 
 function App() {
   return <Login onLogin={(token) => console.log('Logged in mit Token:', token)} />;
